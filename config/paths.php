7418cd5cb5a184ffd943cba0fae0d6384cedc2a7
Dariush099 <dariushn30@gmail.com>
2021-01-07 23:06:12 +0100

    Backup2

--- config/paths.php
+++ config/paths.php
@@ -1,7 +1,7 @@
 <?
 
 // paths
-define('ROOTPATH',          strlen(dirname($_SERVER['SCRIPT_NAME'])) > 1 ? dirname($_SERVER['SCRIPT_NAME']).DIRECTORY_SEPARATOR : DIRECTORY_SEPARATOR);
+define('ROOTPATH',          strlen(dirname($_SERVER['PHP_SELF'])) > 1 ? dirname($_SERVER['PHP_SELF']).DIRECTORY_SEPARATOR : DIRECTORY_SEPARATOR);
 define('VIEWSPATH',         'views'      .DIRECTORY_SEPARATOR);
 define('CONTROLLERSPATH',   'controllers'.DIRECTORY_SEPARATOR);
 define('COREPATH',          'core'       .DIRECTORY_SEPARATOR);
---
+++ views/pages/allProducts.php
@@ -0,0 +1,166 @@
+<section class="menu">
+    <nav class="navbar">
+        <ul class="nav-list">
+            <li class="nav-item">
+                <a href="#" class="nav-link1">Men</a>
+                <ul class="dropdown1">
+                    <li class="dropdown-item">
+                        <a href="#" class="dropdown-link">Schuhe</a>
+                    </li>
+                    <li class="dropdown-item">
+                        <a href="#" class="dropdown-link">Streetwear</a>
+                    </li>
+                    <li class="dropdown-item">
+                        <a href="#" class="dropdown-link">Sale</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link2">Women</a>
+                <ul class="dropdown2">
+                    <li class="dropdown-item">
+                        <a href="#" class="dropdown-link">Schuhe</a>
+                    </li>
+                    <li class="dropdown-item">
+                        <a href="#" class="dropdown-link">Streetwear</a>
+                    </li>
+                    <li class="dropdown-item">
+                        <a href="#" class="dropdown-link">Sale</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</section>
+<section class="mob-menu">
+    <div class="burger">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+    </div>
+    <nav class="mob-nav">
+        <ul class="mob-nav-list">
+            <li class="mob-nav-item">
+                <a href="#" class="mob-nav-link1">Men <div class="angle"> <i class="fas fa-angle-down"></i> </div></a>
+                <ul class="mob-dropdown1">
+                    <li class="mob-dropdown-item">
+                        <a href="#" class="mob-dropdown-link">Schuhe</a>
+                    </li>
+                    <li class="mob-dropdown-item">
+                        <a href="#" class="mob-dropdown-link">Streetwear</a>
+                    </li>
+                    <li class="mob-dropdown-item">
+                        <a href="#" class="mob-dropdown-link">Sale</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="mob-nav-item">
+                <div href="#" class="mob-nav-link2">Women <div class="angle"> <i class="fas fa-angle-down"></i> </div>
+                </div>
+                </a>
+                <ul class="mob-dropdown2">
+                    <li class="mob-dropdown-item">
+                        <a href="#" class="mob-dropdown-link">Schuhe</a>
+                    </li>
+                    <li class="mob-dropdown-item">
+                        <a href="#" class="mob-dropdown-link">Streetwear</a>
+                    </li>
+                    <li class="mob-dropdown-item">
+                        <a href="#" class="mob-dropdown-link">Sale</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="mob-filter">
+                <div class="mob-filter-name">Sortieren:
+                    <div class="mob-filter-box">
+                        <select class="mob-sort-by_f">
+
+                        </select>
+                    </div>
+                </div>
+            </li>
+            <li class="mob-filter">
+                <div class="mob-filter-name">Marke:
+                    <div class="mob-filter-box">
+                        <select class="mob-brand_f">
+
+                        </select>
+                    </div>
+                </div>
+            </li>
+            <li class="mob-filter">
+                <div class="mob-filter-name">Farbe:
+                    <div class="mob-filter-box">
+                        <select class="mob-color_f">
+
+                        </select>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </nav>
+</section>
+<section class="filter-bar">
+    <div class="inset-filter">
+        <div class="filter">Sortieren:
+            <div class="filter-box">
+                <select class="sort-by_f">
+                    <option>Preis</option>
+                </select>
+            </div>
+        </div>
+        <div class="filter">Marke:
+            <div class="filter-box">
+                <select class="brand_f">
+
+                </select>
+            </div>
+        </div>
+        <div class="filter">Farbe:
+            <div class="filter-box">
+                <select class="color_f">
+
+                </select>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="content">
+    <h1 class="title">Schuhe Men:</h1>
+    <div class="row">
+        <?
+        try
+        {
+            $searchfor = isset($_GET['s']) ? 'name like \'%'.(trim($_GET['s'])).'%\' or brand like \'%'.(trim($_GET['s'])).'%\'' : '';
+            $products = null;
+            $products = \dwp\models\Product::select($searchfor);
+            if(empty($products))
+            {
+                echo 'Suche hat keine Ergebnisse.'; #TODO DNA Container mit Anzeige
+            }
+        foreach($products as $product): ?>
+            <a class="link" href="#">
+                <div class="product">
+                    <div class="upper">
+                        <img class="img" src="<?=ROOTPATH?>assets/img/products/product_<?=$product['id']?>.jpg">
+                    </div>
+                    <div class="lower">
+                        <h1 class="brand"><?=$product['brand']?></h1>
+                        <h2 class="model"><?=$product['name']?></h2>
+                        <h3 class="price"><?=$product['price']?>€</h3>
+                    </div>
+                </div>
+            </a>
+        <? endforeach;
+        }   # TODO JGE illegale Suchen abfangen (' OR 1 = 1 OR brand like ' und so weiter)
+        catch (Error $error)
+        {
+            echo 'Error caught: '.$error->getMessage(); # TODO
+        }
+        catch (PDOException $exception)
+        {
+            echo 'Exception caught: '.$exception->getMessage(); # TODO
+        }
+        ?>
+    </div>
+</section>
