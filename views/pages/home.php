f1d0198d8e5930d7ff297dc9facb3eeef49d7f40
Dariush099 <dariushn30@gmail.com>
2021-01-11 21:35:50 +0100

    structured css, design for home.php and login popup

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,15 +1,94 @@
 <?php
     # TODO JGE echo password_hash('123456', PASSWORD_DEFAULT);
 ?>
+<?
+include VIEWSPATH . 'navbar.php';
+?>
+<div class="home">
+    <div class="about-us">
+        <h1 class="about-us-headline">Dein Online-shop für Sneaker und Streetwear</h1>
+        <p class="about-us-text">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>
+    </div>
+    <div class="top-seller">
+        <h1 class="top-seller-headline">Topseller:</h1>
+        <div class="collum">
+            <a class="link" href="#">
+                <div class="product">
+                    <div class="upper">
+                        <img class="img" src="<?=ROOTPATH.'assets/img/products/product_1.jpg'?>">
+                    </div>
+                    <div class="lower">
+                        <h1 class="brand">Reebok</h1>
+                        <h2 class="model">Club C85 Vintage</h2>
+                        <h3 class="price">99,99€</h3>
+                    </div>
+                </div>
+            </a>
+            <a class="link" href="#">
+                <div class="product">
+                    <div class="upper">
+                        <img class="img" src="<?=ROOTPATH.'assets/img/products/product_1.jpg'?>">
+                    </div>
+                    <div class="lower">
+                        <h1 class="brand">Reebok</h1>
+                        <h2 class="model">Club C85 Vintage</h2>
+                        <h3 class="price">99,99€</h3>
+                    </div>
+                </div>
+            </a>
+            <a class="link" href="#">
+                <div class="product">
+                    <div class="upper">
+                        <img class="img" src="<?=ROOTPATH.'assets/img/products/product_1.jpg'?>">
+                    </div>
+                    <div class="lower">
+                        <h1 class="brand">Reebok</h1>
+                        <h2 class="model">Club C85 Vintage</h2>
+                        <h3 class="price">99,99€</h3>
+                    </div>
+                </div>
+            </a>
+            <a class="link" href="#">
+                <div class="product">
+                    <div class="upper">
+                        <img class="img" src="<?=ROOTPATH.'assets/img/products/product_1.jpg'?>">
+                    </div>
+                    <div class="lower">
+                        <h1 class="brand">Reebok</h1>
+                        <h2 class="model">Club C85 Vintage</h2>
+                        <h3 class="price">99,99€</h3>
+                    </div>
+                </div>
+            </a>
+            <a class="link" href="#">
+                <div class="product">
+                    <div class="upper">
+                        <img class="img" src="<?=ROOTPATH.'assets/img/products/product_1.jpg'?>">
+                    </div>
+                    <div class="lower">
+                        <h1 class="brand">Reebok</h1>
+                        <h2 class="model">Club C85 Vintage</h2>
+                        <h3 class="price">99,99€</h3>
+                    </div>
+                </div>
+            </a>
+            <a class="link" href="#">
+                <div class="product">
+                    <div class="upper">
+                        <img class="img" src="<?=ROOTPATH.'assets/img/products/product_1.jpg'?>">
+                    </div>
+                    <div class="lower">
+                        <h1 class="brand">Reebok</h1>
+                        <h2 class="model">Club C85 Vintage</h2>
+                        <h3 class="price">99,99€</h3>
+                    </div>
+                </div>
+            </a>
 
-<div>
-    <h1>
-        HOME
-    </h1>
-    <pre>
-       Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-       At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-       Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-       At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-    </pre>
+        </div>
+
+    </div>
+    <img class="background-img" src="<?=ROOTPATH.'/assets/img/background2.jpeg'?>">
 </div>
