2662c74a74a0a0d8d9407d047ae3cc1797f0f9fe
Dariush099 <dariushn30@gmail.com>
2021-02-17 21:35:03 +0100

    fixed footer, filter options and some other changes

--- views/filter.php
+++ views/filter.php
@@ -3,18 +3,31 @@
         <div class="filter">Sortieren:
             <div class="filter-box">
                 <select class="sort-by_f">
+                    <option class="filter-option">-</option>
+                    <option class="filter-option">Preis aufsteigend</option>
+                    <option class="filter-option">Preis absteigend</option>
+                    <option class="filter-option">Alphabetisch aufsteigend</option>
+                    <option class="filter-option">Alphabetisch absteigend</option>
                 </select>
             </div>
         </div>
         <div class="filter">Marke:
             <div class="filter-box">
                 <select class="brand_f">
+                    <option class="filter-option">Alle</option>
+                    <option class="filter-option">Adidas</option>
+                    <option class="filter-option">Nike</option>
+                    <option class="filter-option">Reebok</option>
                 </select>
             </div>
         </div>
         <div class="filter">Farbe:
             <div class="filter-box">
                 <select class="color_f">
+                    <option class="filter-option">Alle</option>
+                    <option class="filter-option">Weiß</option>
+                    <option class="filter-option">Schwarz</option>
+                    <option class="filter-option">Rot</option>
                 </select>
             </div>
         </div>
@@ -31,6 +44,10 @@
                 <div class="mob-filter-name">Sortieren:
                     <div class="mob-filter-box">
                         <select class="mob-sort-by_f">
+                            <option class="filter-option">Preis aufsteigend</option>
+                            <option class="filter-option">Preis absteigend</option>
+                            <option class="filter-option">Alphabetisch aufsteigend</option>
+                            <option class="filter-option">Alphabetisch absteigend</option>
                         </select>
                     </div>
                 </div>
@@ -39,6 +56,9 @@
                 <div class="mob-filter-name">Marke:
                     <div class="mob-filter-box">
                         <select class="mob-brand_f">
+                            <option class="filter-option">Adidas</option>
+                            <option class="filter-option">Nike</option>
+                            <option class="filter-option">Reebok</option>
                         </select>
                     </div>
                 </div>
@@ -47,6 +67,9 @@
                 <div class="mob-filter-name">Farbe:
                     <div class="mob-filter-box">
                         <select class="mob-color_f">
+                            <option class="filter-option">Weiß</option>
+                            <option class="filter-option">Schwarz</option>
+                            <option class="filter-option">Rot</option>
                         </select>
                     </div>
                 </div>
