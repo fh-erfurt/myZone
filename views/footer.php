231c43866c1dae191f422bdc79c1639e16abafd9
Dariush099 <dariushn30@gmail.com>
2021-02-25 00:44:52 +0100

    docu + thank yoo Page

--- views/footer.php
+++ views/footer.php
@@ -6,9 +6,6 @@
         <a href="<?=$_SERVER['PHP_SELF']?>?c=pages&a=about">Über Uns</a>
     </div>
     <div class="bottom">
-        <a href="<?=$_SERVER['PHP_SELF']?>?c=pages&a=privacyPolicy">Datenschutz</a>
-    </div>
-    <div class="bottom">
         <a href="<?=$_SERVER['PHP_SELF']?>?c=wrongController&a=page3" >ERROR PAGE TEST</a>
     </div>
 </footer>
