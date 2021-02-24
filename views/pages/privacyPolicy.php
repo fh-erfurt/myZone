9622ae46a1c78135d1287f68deb0b95be3000277
Dariush099 <dariushn30@gmail.com>
2021-02-24 20:28:50 +0100

    docu, docu-images, renamed images

--- views/pages/privacyPolicy.php
+++ views/pages/privacyPolicy.php
@@ -1,44 +1,119 @@
-PRIVACY POLICY
-
-<?    $debugMode  = true; # just for readability?>
-<? if($debugMode) : ?>
-    <div style="height:500px">
-        <?
-        echo            '$_SERVER: <textarea>';
-        var_dump($_SERVER);
-        echo '</textarea>$_SESSION: <textarea>';
-        var_dump($_SESSION);
-        echo '</textarea>$sqlErrors: <textarea>';
-        var_dump($sqlErrors ?? 'no sqlErrors');
-        echo '</textarea>Error page cause: <textarea>'.((isset($errCause)) ? $errCause : 'no debug info').'</textarea>';
-        echo '</textarea>$errMsg (remove from code): <textarea>'.((isset($errMsg)) ? $errMsg : 'no $errMsg set (good)').'</textarea>'; # TODO remove
-        ?>
-    </div>
-<? endif; ?>
-
 <div class="doc-box">
-    <h1 class="doc-headline">MyZone Dokumentation</h1>
+    <h1 class="doc-headline">Dokumentation</h1>
     <h2 class="doc-subhead">1. Idee</h2>
-    <p class="idea-text">
-        Aufgrund gemeinsaner Interessen haben wir uns entschieden einen
+    <p>
+        Aufgrund gemeinsaner Interessen haben wir uns entschieden im Rahmen der Varanstaltungen Grundlagen
+        der Webprogrammierung und Dynamische Webprogrammierung einen Streetwear Online-Shop zu erstellen.
+        <h2 class="doc-subsubhead">1.1 Zielgruppe:</h2>
+        Wie für einen Streetwear Online-Shop üblich soll auch unser Dhop vorwiegend modebewusste jugendliche und junge Erwachsene ansprechen, die
+        auf der Suche nach lässiger Alltagskleidung oder bequemen Sneakern sind. Unsere Kunden können auf unserer Shopseite nach Produkten suchen und
+        sie entweder als Gast oder als registrierter Benutzer bestellen.
     </p>
+    <h2 class="doc-subhead">2. Recherche</h2>
+    <p>
+        Um einen Einblick in die Gestaltung und den Funktionsumfang anderer Shopseiten zu erhalten, haben wir uns verschiedene
+        Webshops angeschaut. Nach eingehender Analyse verschiedener Seiten, sind uns folgende Dinge aufgeffale, die wir bei der Umsetzung unseres Projektes beachten wollen:
+    </p>
+    <ul>
+        <li><b>1.</b> Ein schlichtes und modernes Design mit dezenten Farben trägt dazu bei, dass sich die Aufmerksamkeit des Kunden auf die Produkte richtet.</li>
+        <li><b>2.</b> Ansprechende und passende Bilder, die zu unsere Zielgruppe passen, erzeugen eine zusätzliche Bindung mit unserer Seite.</li>
+        <li><b>3.</b> Leicht verständliche und intuitive Bedienung um das Suchen und Kaufen von Produkten so einfach wie möglich zu gestalten.</li>
+    </ul>
+    <div class="website-screenshot-box">
+        <a class="website-link" href="https://www.adidas.de/" target=”_blank”>
+            <img class="screenshot" src="<?=ROOTPATH. '/assets/img/docu-images/AdidasScreenshot.png'?>">
+            <div class="screenshot-info">Adidas Webshop</div>
+        </a>
+        <a class="website-link" href="https://www.hhv.de/shop/de" target=”_blank”>
+            <img class="screenshot" src="<?=ROOTPATH. '/assets/img/docu-images/hhvScreenshot.png'?>">
+            <div class="screenshot-info">Adidas Webshop</div>
+        </a>
+        <a class="website-link" href="https://www.mifcom.de/" target=”_blank”>
+            <img class="screenshot" src="<?=ROOTPATH. '/assets/img/docu-images/MifComScreenshot.png'?>">
+            <div class="screenshot-info">Adidas Webshop</div>
+        </a>
+    </div>
+    <h2 class="doc-subhead">3. Design</h2>
+    <p>
+        <h2 class="doc-subsubhead">3.1 Farben</h2>
+        Wie bereits erwähnt, haben wir uns für ein modernes und schlichtes Design entschieden um unseren
+        Produkten den größtmöglichen Raum zu geben. Auf Grund dessen haben wir hauptsächlich dezente Farben verwendet.
+        Um Buttons oder oder andere wichtige Interaktionen hervorzuheben haben jegliche Buttons und Links einen etwas auffälligen Blauton.
+    </p>
+    <div class="colors">
+        <div class="colorbox">
+            <img src="<?=ROOTPATH. '/assets/img/docu-images/HeaderColor.png'?>">
+            <ul>
+                <li>Header Color</li>
+                <li>Grey 25</li>
+                <li>Hex.: #404040</li>
+                <li>RGB: R64 G64 B64</li>
+                <li>CMYK: C0% M0% Y0% K75%</li>
+            </ul>
+        </div>
+        <div class="colorbox">
+            <img src="<?=ROOTPATH. '/assets/img/docu-images/FooterColor.png'?>">
+            <ul>
+                <li>Footer Color</li>
+                <li>Grey 28</li>
+                <li>Hex.: #dedede</li>
+                <li>RGB: R222 G222 B222</li>
+                <li>CMYK: C0% M0% Y0% K13%</li>
+            </ul>
+        </div>
+        <div class="colorbox">
+            <img src="<?=ROOTPATH. '/assets/img/docu-images/btnColor.png'?>">
+            <ul>
+                <li>Button Color</li>
+                <li>Blue</li>
+                <li>Hex.: #1122db</li>
+                <li>RGB: R17 G34 B219</li>
+                <li>CMYK: C92% M84% Y0% K14%</li>
+            </ul>
+        </div>
+    </div>
+    <p> <h2 class="doc-subsubhead">3.2 Schrift</h2>
+        Als Hausschrift für unseren Webshop haben wir uns für die Schriftart Roboto entschieden. Sie zeichnet sich vor allem durch ihren modernen, grafisch sauberen und gut lesbaren Charakter aus.
+        Somit passt diese Schriftart perfekt zu unserem schlichten und funktionalen Design unserer Webshops.
+    </p>
+    <h2 class="doc-subsubhead">3.3 Layout</h2>
+    <p>Die abgebildeten Wireframes wurden mittels Figma erstellt.</p>
+    <div class="website-screenshot-box">
+        <img class="wireframe-screenshot" src="<?=ROOTPATH. '/assets/img/docu-images/WireframeHomepage.png'?>">
+        <img class="wireframe-screenshot" src="<?=ROOTPATH. '/assets/img/docu-images/WireframeProducts.png'?>">
+        <img class="wireframe-screenshot" src="<?=ROOTPATH. '/assets/img/docu-images/WireframeMobile.png'?>">
+    </div>
+    <p><b>Finales Layout:</b></p>
+    <div class="website-screenshot-box">
+        <img class="wireframe-screenshot" src="<?=ROOTPATH. '/assets/img/docu-images/Layout1.png'?>">
+        <img class="wireframe-screenshot" src="<?=ROOTPATH. '/assets/img/docu-images/Layout2.png'?>">
+        <img class="wireframe-screenshot" src="<?=ROOTPATH. '/assets/img/docu-images/Layout3.png'?>">
+    </div>
+    <p>
+    <h2 class="doc-subsubhead">3.4 Hintergrundbilder:</h2>
+        Die Hintergrundbilder zeigen typische Motive urbaner Hip-Hop Kultur: Street Art, Basketball, Caps und entspannte Alltagskleidung. Unsere Zielgruppe, die mit dieser Kultur aufgewachsen ist, soll sich durch diese Bildauswahl auf unserer Website wohlfühlen und sich angesprochen fühlen.
+    </p>
+    <div class="website-screenshot-box">
+        <img class="wireframe-screenshot" src="<?=ROOTPATH. '/assets/img/BackgroundHomepage.jpeg'?>">
+        <img class="wireframe-screenshot" src="<?=ROOTPATH. '/assets/img/BackgroundProfile.jpg'?>">
+        <img class="wireframe-screenshot" src="<?=ROOTPATH. '/assets/img/BackgroundSignup.jpg'?>">
+    </div>
+    <p>
+        <h2 class="doc-subsubhead">3.5 Responsive Design</h2>
+        Bei der Gestaltung unseres Webshops war uns das responsive Design sehr wichtig, da unsere Zielgruppe vorwiegend mobile Geräte nutzt.
+        Wesentliche Änderungen mussten wir bei der Suchleiste und bei der Filterleiste vornehmen. Diese Funktionen werden bei mobilen Geräten verkleinert dargestellt und vergrößern, wenn man auf das jeweilige Symbol klickt.
+        Für diese Änderungen wurden Checkboxen verwendet.
+    </p>
+    <div class="website-screenshot-box">
+        <img class="wireframe-screenshot" src="<?=ROOTPATH. '/assets/img/docu-images/ResponsiveDesign.png'?>">
+        <img class="wireframe-screenshot" src="<?=ROOTPATH. '/assets/img/docu-images/ResponsiveDesign2.png'?>">
+        <img class="wireframe-screenshot" src="<?=ROOTPATH. '/assets/img/docu-images/ResponsiveDesign3.png'?>">
+    </div>
+    <h2 class="doc-subhead">4. Navigationsstruktur</h2>
+    <img class="page-hierarchy" src="<?=ROOTPATH. '/assets/img/docu-images/Seitenhierachie.png'?>">
+
 </div>
 
-<?
-
-foreach($_SESSION['currentUser'] as $key => $value)
-{
-    echo $key.': _ '.$value.'<br>';
-}
-echo '<br><BR>';
-$address = \dwp\models\Address::selectWhere('id = '.$_SESSION['currentUser']['address'])[0];
-$schema = ['street','number','city','zipCode'];
-foreach($schema as $key)
-{
-    echo $key.': _ '.$address->$key.'<br>';
-}
-
-?>
 
 
 
--- views/pages/thankYou.php
+++ views/pages/thankYou.php
@@ -1,1 +1,50 @@
+<div class="doc-box">
+    <h1 class="doc-headline">MyZone Dokumentation</h1>
+    <h2 class="doc-subhead">1. Idee</h2>
+    <p class="idea-text">
+        Aufgrund gemeinsaner Interessen haben wir uns entschieden einen
+    </p>
+</div>
+
+PRIVACY POLICY
+
+<?    $debugMode  = true; # just for readability?>
+<? if($debugMode) : ?>
+    <div style="height:500px">
+        <?
+        echo            '$_SERVER: <textarea>';
+        var_dump($_SERVER);
+        echo '</textarea>$_SESSION: <textarea>';
+        var_dump($_SESSION);
+        echo '</textarea>$sqlErrors: <textarea>';
+        var_dump($sqlErrors ?? 'no sqlErrors');
+        echo '</textarea>Error page cause: <textarea>'.((isset($errCause)) ? $errCause : 'no debug info').'</textarea>';
+        echo '</textarea>$errMsg (remove from code): <textarea>'.((isset($errMsg)) ? $errMsg : 'no $errMsg set (good)').'</textarea>'; # TODO remove
+        ?>
+    </div>
+<? endif; ?>
+
+<div class="doc-box">
+    <h1 class="doc-headline">MyZone Dokumentation</h1>
+    <h2 class="doc-subhead">1. Idee</h2>
+    <p class="idea-text">
+        Aufgrund gemeinsaner Interessen haben wir uns entschieden einen
+    </p>
+</div>
+
+<?
+
+foreach($_SESSION['currentUser'] as $key => $value)
+{
+    echo $key.': _ '.$value.'<br>';
+}
+echo '<br><BR>';
+$address = \dwp\models\Address::selectWhere('id = '.$_SESSION['currentUser']['address'])[0];
+$schema = ['street','number','city','zipCode'];
+foreach($schema as $key)
+{
+    echo $key.': _ '.$address->$key.'<br>';
+}
+
+?>
 VIELEN DANK FÜR IHREN EINKAUF! !
